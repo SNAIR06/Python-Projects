b0585ba3cc2cb7155108b92d97e6e768278f51fb
Hari Nair <sreeharinair25@gmail.com>
2023-08-09 10:42:57 -0500

    fixed bugs and made sure that user has to type rps.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,18 +1,21 @@
-import random as rng
+import random 
 import os
 
 def clear_console():
-    os.system("clear")
+    os.system("clear") 
 
-def game_beginning():
+def game_beginning(game_choices):
     input("Welcome to Rock, Paper, Scissors! Press any key to continue. ")
-    clear_console()
-    game_choices = ["rock", "paper", "scissors"]
-    comp_choice = rng.choice(game_choices)
+    clear_console() 
+    comp_choice = random.choice(game_choices)
     player_choice = input("Please choose rock, paper, or scissors: ").lower()
     clear_console()
+    while player_choice not in game_choices:
+        player_choice = input("Please choose rock, paper, or scissors: ").lower()
+        clear_console()
 
 def main():
-    game_beginning()
+    game_choices = ["rock", "paper", "scissors"]
+    game_beginning(game_choices)
 
 main()
