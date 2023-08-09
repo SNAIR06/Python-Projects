11e3d6aca39412e8489fac42091a971a351947d3
Hari Nair <sreeharinair25@gmail.com>
2023-08-09 10:29:56 -0500

    turned beginning of game into a function and added a main function which will hold all of the functions and run them.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,14 +4,15 @@ import os
 def clear_console():
     os.system("clear")
 
-input("Welcome to Rock, Paper, Scissors! Press any key to continue. ")
+def game_beginning():
+    input("Welcome to Rock, Paper, Scissors! Press any key to continue. ")
+    clear_console()
+    game_choices = ["rock", "paper", "scissors"]
+    comp_choice = rng.choice(game_choices)
+    player_choice = input("Please choose rock, paper, or scissors: ").lower()
+    clear_console()
 
-clear_console()
+def main():
+    game_beginning()
 
-game_choices = ["rock", "paper", "scissors"]
-
-comp_choice = rng.choice(game_choices)
-
-player_choice = input("Please choose rock, paper, or scissors: ").lower()
-
-print(player_choice)
+main()
