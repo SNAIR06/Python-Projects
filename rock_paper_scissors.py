5689b46f8bf426859e1f79a1422f220bc3050f83
Hari Nair <sreeharinair25@gmail.com>
2023-08-09 11:01:57 -0500

    added situation for when player choose rock, and fixed other bugs with the code.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,17 +5,32 @@ def clear_console():
     os.system("clear") 
 
 def game_beginning(game_choices):
-    input("Welcome to Rock, Paper, Scissors! Press any key to continue. ")
+    input("Welcome to Rock, Paper, Scissors! Press Enter to continue. ")
     clear_console() 
-    comp_choice = random.choice(game_choices)
+    
     player_choice = input("Please choose rock, paper, or scissors: ").lower()
     clear_console()
+    
     while player_choice not in game_choices:
-        player_choice = input("Please choose rock, paper, or scissors: ").lower()
+        player_choice = input("Invalid choice. Please choose rock, paper, or scissors: ").lower()
         clear_console()
+    
+    comp_choice = random.choice(game_choices)  # Move computer choice here
+    return player_choice, comp_choice
 
+def find_winner(player_choice, comp_choice):
+    if player_choice == "rock":
+        if comp_choice == "rock":
+            input("DRAW! Press any key to play again: ")
+        elif comp_choice == "paper":
+            input("YOU LOST! Press any key to play again: ")
+        elif comp_choice == "scissors":
+            input("YOU WON! Press any key to play againL: ")
 def main():
     game_choices = ["rock", "paper", "scissors"]
-    game_beginning(game_choices)
+    player_choice, comp_choice = game_beginning(game_choices) 
+    
+    find_winner(player_choice, comp_choice)
 
 main()
+
